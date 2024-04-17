3d0a50a30bd47a479b7c8da7a6344ee13951dad6
ludvikkrocak <142713809+ludvikkrocak@users.noreply.github.com>
2024-04-17 20:20:46 +0200

    Add new select

--- export.sql
+++ export.sql
@@ -25,16 +25,43 @@ INNER JOIN
         ,value AS export_usd
     FROM czech_export
     WHERE element = 'Export Value' AND value <> 0) v
-ON q.partner_countries = v.partner_countries AND q.item = v.item AND q.year = v.year;
+ON q.partner_countries = v.partner_countries AND q.item = v.item AND q.year = v.year
+GROUP BY year, partner_countries;
 
 
 SELECT * FROM tbl_czech_export;
 
 
-SELECT 
-    year,
+SELECT
     SUM(percentage_export_tonnes) AS total_percentage,
     SUM(percentage_export_usd) AS total_percentage_usd
 FROM 
     tbl_czech_export
 WHERE year = 2000;
+
+-- CREATE VIEW country_percentage AS
+SELECT 
+		year
+		,partner_countries
+		,percentage_export_tonnes AS country_percentage_export_tonnes
+		,percentage_export_usd AS country_percentage_export_usd
+FROM 
+    tbl_czech_export
+GROUP BY year, partner_countries;
+
+
+SELECT 
+		ce.year
+		,ce.partner_countries
+		,ce.item
+		,export_tonnes
+		,export_usd
+		,country_percentage_export_tonnes
+		,country_percentage_export_usd
+--		,SUM(percentage_export_tonnes) AS total_percentage
+FROM tbl_czech_export ce
+LEFT JOIN country_percentage cp
+	ON ce.year = cp.year AND ce.partner_countries = cp.partner_countries
+WHERE ce.year = 2000;
+		
+
